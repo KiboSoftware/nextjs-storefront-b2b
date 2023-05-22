3c3b4a1390bbf4676f27050d3944ba1c4c238bc3
Krishna Mohan <krishna.m@ignitiv.com>
2023-05-22 11:15:17 +0530

    feature: add Pagination component

--- components/common/Pagination/Pagination.tsx
+++ components/common/Pagination/Pagination.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Pagination, createTheme, ThemeProvider, SxProps, Theme } from '@mui/material'
+import { Pagination, createTheme, ThemeProvider, SxProps, Theme, useTheme } from '@mui/material'
 import { Container } from '@mui/system'
 
 interface PaginationProps {
@@ -10,34 +10,25 @@ interface PaginationProps {
   sx?: SxProps<Theme>
 }
 
-const customTheme = createTheme({
-  palette: {
-    primary: {
-      main: '#2B2B2B',
-    },
-  },
-})
-
 export default function PaginationCustom(props: PaginationProps) {
   const { count, size, sx } = props
-
+  const theme = useTheme()
   function handleChange(e: any, value: any) {
     props.onChange(value)
   }
 
   return (
     <>
-      <ThemeProvider theme={customTheme}>
-        <Container style={{ margin: '20px 0' }}>
-          <Pagination
-            count={count}
-            shape={`rounded`}
-            color="primary"
-            onChange={handleChange}
-            size={size}
-            sx={{ ...sx }}
-          />
-        </Container>
+      <ThemeProvider theme={theme}>
+        <Pagination
+          count={count}
+          shape={`rounded`}
+          color="primary"
+          onChange={handleChange}
+          size={size}
+          sx={{ ...sx }}
+          variant="text"
+        />
       </ThemeProvider>
     </>
   )
