a0b604345653db57be861e03d518dbef1405f851
kibo-chandradeeptalaha <89371824+kibo-chandradeeptalaha@users.noreply.github.com>
2023-08-30 22:46:36 +0530

    feat: adding test cases

--- components/b2b/AccessWrapper/AccessWrapper.tsx
+++ components/b2b/AccessWrapper/AccessWrapper.tsx
@@ -3,21 +3,29 @@ import React from 'react'
 import { useAuthContext } from '@/context'
 import { B2BRoles, QuoteStatus } from '@/lib/constants'
 
-const isAdminOrPurchaser = B2BRoles.ADMIN || B2BRoles.PURCHASER
+const handleEditQuoteAccess = (role: string, quoteStatus?: string) => {
+  return (
+    (role === B2BRoles.ADMIN || role === B2BRoles.PURCHASER) &&
+    (quoteStatus === QuoteStatus.Pending || quoteStatus === QuoteStatus.ReadyForCheckout)
+  )
+}
+
+const handleEmailQuoteAccess = (role: string, quoteStatus?: string) => {
+  return (
+    (role === B2BRoles.ADMIN || role === B2BRoles.PURCHASER) &&
+    (quoteStatus === QuoteStatus.Pending ||
+      quoteStatus === QuoteStatus.InReview ||
+      quoteStatus === QuoteStatus.ReadyForCheckout ||
+      quoteStatus === QuoteStatus.Expired)
+  )
+}
 
 const AccessManager: any = (role: string, quoteStatus?: string, quoteMode?: string) => {
-  const { Pending, InReview, ReadyForCheckout, Completed, Expired } = QuoteStatus
   return {
     QuoteAddShippingAddress: role === B2BRoles.ADMIN,
-    DeleteQuote: isAdminOrPurchaser,
-    EditQuote:
-      role === isAdminOrPurchaser && (quoteStatus === Pending || quoteStatus === ReadyForCheckout),
-    EmailQuote:
-      isAdminOrPurchaser &&
-      (quoteStatus === Pending ||
-        quoteStatus === InReview ||
-        quoteStatus === ReadyForCheckout ||
-        quoteStatus === Expired),
+    DeleteQuote: role === B2BRoles.ADMIN || role === B2BRoles.PURCHASER,
+    EditQuote: handleEditQuoteAccess(role, quoteStatus),
+    EmailQuote: handleEmailQuoteAccess(role, quoteStatus),
   }
 }
 
@@ -25,14 +33,14 @@ interface AccessWrapperProps {
   name: string
   quoteStatus?: string
   quoteMode?: string
-  children: React.ReactNode
+  children: any
 }
 
 // wrap it around any Node that needs to be conditionally rendered based on user role, quote status, or quote mode
 const AccessWrapper = (props: AccessWrapperProps) => {
   const { name, quoteStatus, quoteMode } = props
   const { user } = useAuthContext()
-  const shouldShow = AccessManager[name](user?.roleName, quoteStatus, quoteMode)
+  const shouldShow = AccessManager(user?.roleName, quoteStatus, quoteMode)[name]
 
   return shouldShow ? props.children : null
 }
