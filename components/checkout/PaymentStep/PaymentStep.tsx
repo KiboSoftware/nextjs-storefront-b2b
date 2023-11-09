c0365c2d9ccfb4a0f11ab5ba7728473cd21adc47
Kibo-TriptiV <122886510+Kibo-TriptiV@users.noreply.github.com>
2023-11-09 16:19:10 +0530

    reverted icky-923 hide single payment type radio if data present

--- components/checkout/PaymentStep/PaymentStep.tsx
+++ components/checkout/PaymentStep/PaymentStep.tsx
@@ -765,18 +765,12 @@ const PaymentStep = (props: PaymentStepProps) => {
           {newPaymentTypes.map((paymentType: PaymentsType) => {
             return (
               <Box key={paymentType.id}>
-                {(newPaymentTypes?.length > 1 ||
-                  !(
-                    shouldShowPreviouslySavedCards ||
-                    shouldShowPreviouslySavedPaymentsForPurchaseOrder
-                  )) && (
-                  <FormControlLabel
-                    sx={{ ...formControlLabelStyle }}
-                    value={paymentType.id}
-                    control={<Radio sx={{ ...radioStyle }} />}
-                    label={paymentType.name}
-                  />
-                )}
+                <FormControlLabel
+                  sx={{ ...formControlLabelStyle }}
+                  value={paymentType.id}
+                  control={<Radio sx={{ ...radioStyle }} />}
+                  label={paymentType.name}
+                />
                 {paymentType.id === selectedPaymentTypeRadio ? (
                   <Box sx={{ maxWidth: '100%', mb: 1 }}>
                     {shouldShowPreviouslySavedCards ? (
